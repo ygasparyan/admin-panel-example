82691a4ab5f323a37b11013349de2e005103ab24
ygasparyan <ygasparyan12@gmail.com>
2019-11-30 23:21:23 +0400

    feat: generated modules and components

---
+++ src/app/resource-item/resource-item.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResourceItemComponent } from './resource-item.component';
+
+@NgModule({
+  declarations: [ResourceItemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ResourceItemModule { }
