a32ee60a22fc39d35a4aadceb49b1143f0d1c901
ygasparyan <ygasparyan12@gmail.com>
2019-12-02 19:47:08 +0400

    feat: show stats in charts component

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../services';
 
 @Component({
   selector: 'app-charts',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./charts.component.scss']
 })
 export class ChartsComponent implements OnInit {
+  public data;
+  // options for the chart
+  public view: any[] = [700, 400];
+  public showXAxis = true;
+  public showYAxis = true;
+  public showLegend = true;
+  public showLabels = true;
 
-  constructor() { }
+  constructor(
+    private statsService: StatsService
+  ) { }
 
   ngOnInit() {
+    this.data = this.statsService.getStats();
   }
 
 }
--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsComponent } from './charts.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { MatDividerModule } from '@angular/material';
 
 @NgModule({
   declarations: [ChartsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxChartsModule,
+    MatDividerModule
   ]
 })
 export class ChartsModule { }
